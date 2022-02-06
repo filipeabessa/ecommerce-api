92421bf04c4a9ecc50fb2357838136f253a85e43
filipeabessa <filipegbessa@gmail.com>
2022-02-06 18:48:20 -0300

    feat(product): :sparkles: add query params to the get products request

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -6,11 +6,8 @@ class Product(models.Model):
     responsible = models.ForeignKey('auth.User', related_name='products', on_delete=models.CASCADE)
     creation = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
     
-    
-    
-
     def __str__(self):
         return self.title
     
--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,8 +1,25 @@
 from rest_framework import viewsets
+from decimal import Decimal
+
+
+
 
 from .models import Product
 from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        title = self.request.query_params.get('title', None)
+        price = self.request.query_params.get('price', None)
+        
+        if (title is not None):
+            queryset = queryset.filter(title__icontains=title)
+            
+        if (price is not None):
+            queryset = queryset.filter(price=Decimal(price))
+        
+        return queryset
+    
